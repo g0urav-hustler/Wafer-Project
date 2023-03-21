242461281ae976a08cdc7bd2c8ba079a4a178bb0
g0urav-hustler <gouravchouhan966@gmail.com>
2023-03-21 17:17:28 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='It is the project to predict the wafer is correct using sensor signals.',
+    author='Gourav_Chouhan',
+    license='MIT',
+)
